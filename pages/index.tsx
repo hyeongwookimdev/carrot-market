1a7279afd5e65c248935199490ac9ed42f84e715
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-12-17 19:10:50 +0900

    📝 Practice Modifiers for Forms in TailWind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,31 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
+    <form className="flex flex-col space-y-2 p-5">
+      <input
+        type="text"
+        required
+        placeholder="Username"
+        className="border p-1 peer border-gray-400 rounded "
+      />
+      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
+        This input is invalid
+      </span>
+      <span className="hidden peer-valid:block peer-valid:text-blue-500">
+        Awesome username!
+      </span>
+      <span className="hidden peer-hover:block peer-valid:text-teal-500">
+        Hello!
+      </span>
+      <input type="submit" value="Login" className="bg-white" />
+    </form>
+  );
+};
+
+export default Home;
+
+/*
+<div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <ul>
@@ -84,7 +108,4 @@ const Home: NextPage = () => {
         </div>
       </div>
     </div>
-  );
-};
-
-export default Home;
+*/
