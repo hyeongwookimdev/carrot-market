73e67234cf67444e5fd04158c966647f9b79346d
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-12-26 18:59:23 +0900

    ✨  Add Account Logic to api/users/enter.tsx

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,22 @@
+import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { email, phone } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "알 수 없는 유저",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+
   return res.json({ ok: true });
 }
 
