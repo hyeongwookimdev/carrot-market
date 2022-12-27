9fb8e41173e3ff1459ce1315e49419ce70a5906c
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-12-27 09:05:09 +0900

    🚀📝 Set Up Twilio & Add twilioClient

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,17 @@
+import twilio from "twilio";
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { email, phone } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -22,8 +29,17 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
-  return res.json({ ok: true });
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.TWILIO_MY_PHONE!, // phone
+      body: `당신의 로그인 토큰은 ${payload} 입니다.`,
+    });
+    console.log(message);
+  }
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
